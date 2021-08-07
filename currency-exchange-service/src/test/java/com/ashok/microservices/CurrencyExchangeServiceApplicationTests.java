e56bed4376edd6e97c8fe511ba5dbae1a907a18b
Ashok Bhagat <ashoka.bhagat@gmail.com>
2021-08-07 21:38:07 +0530

    currency exchange microservice

---
+++ currency-exchange-service/src/test/java/com/ashok/microservices/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashok.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
