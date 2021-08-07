858bd77640b4402526645023596a0fff33c25e0a
Ashok Bhagat <ashoka.bhagat@gmail.com>
2021-08-07 23:11:37 +0530

    enabled jpa

--- currency-exchange-service/src/main/java/com/ashok/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/ashok/microservices/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.ashok.microservices.controller;
 
 import com.ashok.microservices.bean.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,16 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
-    @GetMapping(path="/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to){
-        return new CurrencyExchange(100L, "INR", "EURO" , BigDecimal.TEN);
+    @Autowired
+    private Environment environment;
+
+    @GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = new CurrencyExchange(100L, "INR", "EURO", BigDecimal.TEN);
+
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+
+        return currencyExchange;
     }
 
 }
