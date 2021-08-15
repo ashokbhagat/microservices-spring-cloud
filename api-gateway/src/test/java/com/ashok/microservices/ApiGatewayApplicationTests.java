734bca4b01a1d96c2b027a0a8aa90114426b5f9e
Ashok Bhagat <ashoka.bhagat@gmail.com>
2021-08-15 19:01:51 +0530

    API Gateay

---
+++ api-gateway/src/test/java/com/ashok/microservices/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashok.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
